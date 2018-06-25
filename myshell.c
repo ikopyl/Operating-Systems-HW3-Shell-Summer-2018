20a960865d4c3320e640227ecd39e1c99887b325
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-06-25 15:07:38 -0700

    Building a PoC of the shell prompt (creating an infinite loop terminated by Ctrl+D

--- myshell.c
+++ myshell.c
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 //Max amount allowed to read from input
 #define BUFFERSIZE 256
@@ -23,9 +24,40 @@
 //sizeof shell prompt
 #define PROMPTSIZE sizeof(PROMPT)
 
+void verify_memory_allocation(size_t);
+void check_for_errors(ssize_t, char *);
+
 int main(int* argc, char** argv)
 {
+    char *buf = (char *) calloc(BUFFERSIZE, sizeof(char));
+    verify_memory_allocation((size_t) buf);
 
-    
-return 0;
+    ssize_t bytes_read = 0;
+    ssize_t bytes_written = 0;
+
+    while ((bytes_read = read(0, buf, BUFFERSIZE)))
+    {
+        bytes_written = write(1, buf, bytes_read);
+        check_for_errors(bytes_written, "Write error...");
+    }
+    check_for_errors(bytes_read, "Read error...");
+
+
+    return 0;
 }
+
+void check_for_errors(ssize_t status_code, char * error_message) {
+    if (status_code < 0) {
+        perror(error_message);
+        exit(errno);
+    }
+}
+
+void verify_memory_allocation(size_t ptr)
+{
+    if (!ptr) {     // i.e. if ptr <= 0
+        perror("Unable to allocate dynamic memory...");
+        exit(errno);
+    }
+}
+
